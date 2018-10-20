a93bcfa5f2870b2637344d7fb9a91a868c89e68e
gomaomao <gomaomao.com@gmail.com>
2018-10-20 12:01:04 +0800

    增加下载地址和歌曲封面

--- kugou-web-crawler.py
+++ kugou-web-crawler.py
@@ -11,6 +11,8 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+import re
+import json
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
@@ -29,13 +31,33 @@ def get_info(url):
         songTime = songTime.getText().strip()
         count += 1
         print('歌曲{count}：\n'.format(count=count)+'链接：' + href + '\n' + '歌曲：' + singer + '\n' + '时长：' + songTime + '\n')
-        file.write('歌曲{count}：\n'.format(count=count) + '链接：' + href + '\n' + '歌曲：' + singer + '\n' + '时长：' + songTime + '\n' + '\n')
+        file.write('歌曲{count}：\n'.format(count=count) + '链接：' + href + '\n' + '歌曲：' + singer + '\n' + '时长：' + songTime + '\n')
+        get_hash(href)
+
+def get_hash(url):
+    res = requests.get(url, headers=headers)
+    soup = BeautifulSoup(res.text, "html.parser")
+    hash_pattern = re.compile(r"\"hash\"\:\"(.*?)\"", re.MULTILINE | re.DOTALL)
+    script = soup.find("script")
+    hash = hash_pattern.search(script.text).group(1)
+    get_music(hash)
+
+def get_music(hash):
+    global count, file
+    res = requests.get('http://wwwapi.kugou.com/yy/index.php?r=play/getdata&callback=jQuery19106946809784755619_1540004931600&hash=' + hash + '&album_id=0')
+    music = json.loads(res.text.replace('jQuery19106946809784755619_1540004931600(','').replace(');',''))
+    print('hash：' + hash + '\n' + '下载地址：' + music['data']['play_url'] + '\n' + '封面：' + music['data']['img'] + '\n')
+    file.write('hash：' + hash + '\n' + '下载地址：' + music['data']['play_url'] + '\n' + '封面：' + music['data']['img'] + '\n' + '\n')
+    if music['data']['play_url']:
+        r = requests.get(music['data']['play_url']) 
+        with open(r"music/" + music['data']['audio_name'] + ".mp3", "wb") as code:
+            code.write(r.content)    
 
 def main():
     global count, file
     count = 0
     # UnicodeEncodeError 坑了半小时，最后使用 open() 函数时加上 encoding='utf-8' 这个参数解决了问题
-    file = open(r'E:\AllPrj\PyCharmPrj\py-crawler\results-kugou-top500.txt', 'w', encoding='utf-8')
+    file = open(r'results-kugou-top500.txt', 'w', encoding='utf-8')
     # 原先的写法：
     # file = open(r'E:\AllPrj\PyCharmPrj\py-crawler\results-kugou-top500.txt', 'w')
     # 运行报错如下：
@@ -56,7 +78,7 @@ def main():
         print('on page:'+url+'\n')
         file.write('on page:'+url+'\n')
         get_info(url)
-        time.sleep(1)
+        time.sleep(20)
 
 if __name__ == '__main__':
     main()
